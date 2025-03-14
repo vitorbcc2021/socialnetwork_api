f380f850e63eb9ed2197dbcb958f57428758ff6f
Vitor Allace <vitorallacepnn@gmail.com>
2025-03-14 18:13:14 -0300

    uploading current project

---
+++ src/main/java/com/vitor/socialnetwork_api/SocialnetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.socialnetwork_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkApiApplication.class, args);
+	}
+
+}
